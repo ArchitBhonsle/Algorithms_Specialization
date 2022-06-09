3966fd424bd4e57a077c44f84f84603ba585a6a6
Archit Bhonsle <abhonsle2000@gmail.com>
2022-06-09 11:31:49 +0530

    week four: double counting fix

--- course_one/src/week_four/karger_mincut.rs
+++ course_one/src/week_four/karger_mincut.rs
@@ -21,6 +21,7 @@ impl Graph {
 
             adj.push(iter.collect::<Vec<usize>>());
         });
+
         Graph { adj }
     }
 
@@ -40,7 +41,6 @@ impl Graph {
     pub fn contract_edge(&mut self, i: usize) {
         let (a, b) = self.get_edge_vertices(i);
         let (a, b) = (usize::min(a, b), usize::max(a, b));
-        // dbg!((a, b), &self.adj);
 
         let b_list = self.adj.remove(b);
         self.adj[a].extend(b_list);
@@ -59,18 +59,17 @@ impl Graph {
 
     pub fn kargers_min_cut(&mut self) -> usize {
         let mut rng = rand::thread_rng();
-        // dbg!();
         while self.adj.len() > 2 {
             let m = self.adj.iter().map(|x| x.len()).sum();
             let rand = rng.gen_range(0..m);
-            // dbg!(rand);
 
             self.contract_edge(rand);
         }
 
-        // dbg!(&self);
-
-        dbg!(self.adj.iter().flatten().count())
+        // because each edge is represented twice, once in the 
+        // adjacency list of each of its vertices and thus each 
+        // crossing edge is counted twice.
+        self.adj.iter().flatten().count() / 2
     }
 }
 
@@ -111,33 +110,28 @@ mod tests {
         helper(11, (3, 2));
     }
 
-    const E2: &str = r"1 2 3 4
-2 3 4
-3 4";
-
     #[test]
     fn test_contraction() {
-        let mut graph = Graph::new_from_str(E2);
+        let mut graph = Graph::new_from_str(E1);
         graph.contract_edge(0);
-        dbg!(&graph);
 
         let exp = Graph {
-            adj: vec![vec![1, 2, 1, 2], vec![2]],
+            adj: vec![vec![1, 2, 1, 2], vec![0, 0, 2], vec![0, 0, 1]],
         };
 
         assert_eq!(graph, exp);
     }
 
     const E3: &str = r"1 2 3 4
-2 3 4
-3 4 5
-4 6
-5 6 7 8
-6 7 8
-7 8
-8";
+2 1 3 4
+3 1 2 4 5
+4 1 2 3 6
+5 3 6 7 8
+6 4 5 7 8
+7 5 6 8
+8 5 6 7";
 
-    #[test]
+   #[test]
     fn test_kargers_min_cut() {
         let graph = Graph::new_from_str(E3);
 
@@ -166,10 +160,6 @@ mod tests {
                 dbg!((i, curr, res));
             }
         }
-        // let res = (0..graph.adj.len().pow(2))
-        //     .map(|_| graph.clone().kargers_min_cut())
-        //     .min()
-        //     .unwrap();
 
         dbg!(res);
     }
